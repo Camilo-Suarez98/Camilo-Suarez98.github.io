a0248d5d77f76ed7fff2832866d18c7806362e3f
Camilo S <68169750+Camilo-Suarez98@users.noreply.github.com>
2023-01-10 22:19:33 -0500

    Add files via upload
    Changes in the components

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 const Button = () => {
     return (
-        <Link href="https://docs.google.com/document/d/1S0rfwHJrzH1D03FKCZl4F0AypJ7O3dMbvbn0EljOvXM/edit?usp=sharing">
-            <a className="bg-blue-600 w-44 h-8 text-center leading-8 rounded-md text-blue-800 font-bold hover:text-white active:transform hover:translate-y-1" rel="noreferrer" target="_blank">
+        <Link href="https://docs.google.com/document/d/1pCMX9uTb3b7oc-cO1TdAweR5nXJfAmJK/edit?usp=sharing&ouid=111647670086009394452&rtpof=true&sd=true">
+            <a className="bg-blue-800 w-44 h-8 text-center leading-8 rounded-md text-blue-50 font-bold transition-all duration-500 hover:text-gray-500 active:transform hover:translate-y-1" rel="noreferrer" target="_blank">
                 Download my CV
             </a>
         </Link>
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,64 +1,80 @@
 import Head from "next/head"
 import Link from "next/link"
 import Image from "next/image"
-import logo from "../public/letterc.jpg"
+import logo from "../public/icono-portf.png"
+import React, { useContext, useState } from "react"
+import {ThemeContext} from "../context/ThemeContext"
 
 const Layout = ({ title, children }) => {
+  const themeMode = useContext(ThemeContext)
+  const [dark, setDark] = useState(themeMode)
+
   return(
     <div>
       <Head>
         <title>{title} - Camilo S</title>
-        <link rel="icon" href="/letterc.jpg" />
-        <link 
-          rel="stylesheet" 
-          href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" 
-          integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" 
-          crossorigin="anonymous" />
-          <link rel="stylesheet" href="style.css" />
+        <link rel="icon" href="/icono-portf.png" />
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+          integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
+          crossOrigin="anonymous"
+        />
+        <link rel="stylesheet" href="style.css" />
       </Head>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 mb-10">
-        <div className="container px-4 mx-auto flex items-center justify-between sm:flex-col sm:justify-center">
-            <div className="flex justify-start item-center w-full relative px-4 sm:w-auto sm:static sm:justify-center">
-              <Image className="image" src={logo} alt="Page's logo" width={30} height={20} />
-              <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white">
-                Camilo Suarez
+      <div className={dark === '🌞' ? '🌚' : '🌞'}>
+        <nav className="w-full relative flex flex-wrap items-center justify-between px-2 py-3 mb-10">
+          <div className="container px-4 mx-auto flex items-center justify-between sm:flex-col sm:justify-center">
+            <div className="relative px-4 sm:w-auto sm:static sm:justify-center">
+              <a href="/" className="flex items-center w-40 text-sm font-bold leading-relaxed cursor-pointer inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-800">
+                <Image className="image" src={logo} alt="Page's logo" width={40} height={40} />
+                Camilo Suárez
               </a>
             </div>
-              <button className="burger absolute right-0 top-5 cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent hidden outline-none focus:outline-none" type="button">
-                <span className="block relative w-6 h-px rounded-sm bg-white"></span>
-                <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
-                <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
-              </button>
-          <div className="flex flex-grow items-center sm:justify-center sm:text-center" id="example-navbar-warning">
-            <ul className="flex list-none ml-auto sm:flex-col sm:justify-start">
-              <li className="nav-item">
-                <Link href="/" title="Home">
-                  <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75">
-                    Home
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/portfolio" title="Portfolio">
-                  <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75">
-                    Portfolio
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/contact" title="Contact">
-                  <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75">
-                    Contact
-                  </a>
-                </Link>
-              </li>
-            </ul>
+            <button onClick={() => {
+              setDark(dark === '🌞' ? '🌚' : '🌞')
+              }}
+            >
+              {dark}
+            </button>
+            
+            <button className="burger absolute right-0 top-5 cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent hidden outline-none focus:outline-none" type="button">
+              <span className="block relative w-6 h-px rounded-sm bg-white"></span>
+              <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+              <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+            </button>
+
+            <div className="flex flex-grow items-center sm:justify-center sm:text-center" id="example-navbar-warning">
+              <ul className="flex list-none ml-auto sm:flex-col sm:justify-start">
+                <li className="nav-item">
+                  <Link href="/" title="Home">
+                    <a className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-gray-800 hover:opacity-75">
+                      Home
+                    </a>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link href="/portfolio" title="Portfolio">
+                    <a className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-gray-800 hover:opacity-75">
+                      Portfolio
+                    </a>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link href="/contact" title="Contact">
+                    <a className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-gray-800 hover:opacity-75">
+                      Contact
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
+        </nav>
+        <div className="w-full h-full flex flex-col justify-evenly">
+          {children}
         </div>
-      </nav>
-      <div className="w-full bg-blue-500 h-full flex flex-col justify-evenly">
-        {children}
-      </div>
+      </div>     
     </div>
   )
 }

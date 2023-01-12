630e556c532327dadf2cc5f9467eb663573867ae
Camilo S <68169750+Camilo-Suarez98@users.noreply.github.com>
2023-01-11 20:55:49 -0500

    Add files via upload
    Changes in the pages

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,12 +1,17 @@
 import Layout from "../components/Layout"
-import Projects from "../components/Projects"
+import QueryProvider from "../components/Projects"
 
 const portfolio = () => {
     return(
         <>
             <Layout title="Portfolio">
-                <h1 className="font-semibold text-4xl text-gray-700 text-center pb-8">Projetcs</h1>
-                <Projects />
+                <h1 className="font-semibold text-4xl text-gray-700 text-center pb-8">Projects</h1>
+                <QueryProvider />
+                <a href="#top" className="fixed right-9 bottom-5 border-2 border-gray-400 rounded-full ease-in transition-all duration-300">
+                    <span className="material-symbols-outlined flex">
+                        arrow_upward
+                    </span>
+                </a>
             </Layout>
         </>
     )

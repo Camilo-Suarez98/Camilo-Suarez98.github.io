7f46228f616be843d68b55cf299d955fc69eb55c
Camilo S <68169750+Camilo-Suarez98@users.noreply.github.com>
2023-01-11 20:55:09 -0500

    Add files via upload
    Change in the context

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -1,17 +1,20 @@
 import { createContext, useState } from "react";
 
-const ThemeContext = createContext('🌚')
+const ThemeContext = createContext()
 
-function DarkModeProvider({children}) {
-  const [darkMode, setDarkMode] = useState('🌚')
+function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState('light')
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
 
   return (
     <>
-      <ThemeContext.Provider value={darkMode}>
-        {children}
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        { children }
       </ThemeContext.Provider>
     </>
   )
 }
 
-export {DarkModeProvider, ThemeContext}
+export { ThemeProvider, ThemeContext }

9e631a4d78c6ed599355da4c1603bdecb006bea5
Camilo S <68169750+Camilo-Suarez98@users.noreply.github.com>
2023-01-13 21:07:21 -0500

    Add files via upload
    Uploading changes

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head />
-      <body className='bg-blue-50 h-screen'>
+      <body>
         <Main />
         <NextScript />
       </body>

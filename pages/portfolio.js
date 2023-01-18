454a927ff799340f5871a7858cc10f7ad9b786f2
Camilo S <68169750+Camilo-Suarez98@users.noreply.github.com>
2023-01-17 22:14:30 -0500

    Add files via upload
    Making some changes

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,13 +1,18 @@
 import Layout from "../components/Layout"
-import QueryProvider from "../components/Projects"
+import { QueryClient, QueryClientProvider } from "react-query"
+import Projects from "../components/Projects"
+
+const queryClient = new QueryClient()
 
 const portfolio = () => {
-    return(
+    return (
         <>
             <Layout title="Portfolio">
                 <h1 className="font-semibold text-4xl text-gray-700 text-center pb-8">Projects</h1>
-                <QueryProvider />
-                <a href="#top" className="fixed flex justify-center items-center w-7 h-7 border-2 border-gray-400 rounded-full right-9 bottom-5 animate-bounce w-6 h-6 ease-in transition-all duration-300 sm:right-4 sm:bottom-2.5 hover:bg-blue-700">
+                <QueryClientProvider client={queryClient}>
+                    <Projects />
+                </QueryClientProvider>
+                <a href="#top" className="circle-arrow fixed flex justify-center items-center w-9 h-9 rounded-full right-9 bottom-5 animate-bounce ease-in transition-all ease-in-out duration-300 sm:right-4 sm:bottom-2.5 hover:bg-blue-700">
                     <span className="material-symbols-outlined flex">
                         arrow_upward
                     </span>

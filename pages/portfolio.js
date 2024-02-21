bf151fc9523c2a6a4e37bc36b35d1e48e2ac40d7
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-02-21 10:39:07 -0500

    changes tab spaces to 2

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -5,21 +5,21 @@ import Projects from "../components/Projects";
 const queryClient = new QueryClient();
 
 const portfolio = () => {
-    return (
-        <>
-            <Layout title="Portfolio">
-                <h1 className="font-semibold text-4xl text-gray-700 text-center mb-8">Projects</h1>
-                <QueryClientProvider client={queryClient}>
-                    <Projects />
-                </QueryClientProvider>
-                <a href="#top" className="circle-arrow fixed flex justify-center items-center w-9 h-9 rounded-full right-9 bottom-5 animate-bounce ease-in transition-all duration-300 sm:right-4 sm:bottom-2.5 hover:bg-blue-700">
-                    <span className="material-symbols-outlined flex">
-                        arrow_upward
-                    </span>
-                </a>
-            </Layout>
-        </>
-    );
+  return (
+    <>
+      <Layout title="Projects">
+        <h1 className="font-semibold text-4xl text-gray-700 text-center mb-8">Projects</h1>
+        <QueryClientProvider client={queryClient}>
+          <Projects />
+        </QueryClientProvider>
+        <a href="#top" className="circle-arrow fixed flex justify-center items-center w-9 h-9 rounded-full right-9 bottom-5 animate-bounce ease-in transition-all duration-300 sm:right-4 sm:bottom-2.5 hover:bg-blue-700">
+          <span className="material-symbols-outlined flex">
+            arrow_upward
+          </span>
+        </a>
+      </Layout>
+    </>
+  );
 };
 
 export default portfolio;

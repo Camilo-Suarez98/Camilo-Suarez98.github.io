5e96cb84da4ecf85c497e50013b8c3908f9651cc
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-02-21 14:41:26 -0500

    edit info main page

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,11 @@ export default function Home() {
         <div className="flex flex-col justify-around items-center mt-20 h-80 ls:h-5/6 w-full md:mt-16 lg:mt-24 2xl:mt-20">
           <h1 className="font-semibold text-center mb-12 text-4xl text-gray-700 md:mb-4">Hi!👋 I'm Camilo</h1>
           <p className="text-2xl text-gray-500 pb-12 text-center">
-
-            I'm a Dedicated FullStack Developer with a deep passion for frontend development based in Bucaramanga, Col 🇨🇴.
+            I'm a Dedicated Frontend Developer based in Bucaramanga, Col 🇨🇴.
             <br />
-            🚀 I specialize in crafting exceptional web experiences, with a focus on user-centric design and robust functionality. My skill set includes:
+            🚀 I specialize in crafting exceptional web experiences, with a focus on user-centric design and robust functionality. My skill set includes: HTML, CSS, JavaScript, React, Next, GIT, TailwindCSS, styled-components and SASS.
             <br />
-            🌱 I'm on a constant journey of learning and self-improvement, always aiming to be a better programmer. Currently, I'm seeking new opportunities that challenge and expand my skills as a Fullstack Developer with a strong Frontend passion.
+            🌱 I'm on a constant journey of learning and self-improvement, always aiming to be a better programmer. Currently, I'm seeking new opportunities that challenge and expand my skills as a Frontend Developer.
           </p>
           <Button />
         </div>

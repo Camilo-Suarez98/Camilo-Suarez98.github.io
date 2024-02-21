bcbf873f6038c999228f11dbd85932061e720865
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-02-21 10:40:36 -0500

    create loader component to use in loading situations

---
+++ components/Loader.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className="terminal-loader">
+      <div className="terminal-header">
+        <div className="terminal-title">Status</div>
+        <div className="terminal-controls">
+          <div className="control close"></div>
+          <div className="control minimize"></div>
+          <div className="control maximize"></div>
+        </div>
+      </div>
+      <div className="text">Loading...</div>
+    </div>
+  );
+};
+
+export default Loader;

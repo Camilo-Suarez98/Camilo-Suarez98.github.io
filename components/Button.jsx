42fd70aece01f4fa69667936bc92f4f23b51e875
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-02-22 10:25:34 -0500

    create new button component

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,19 +1,13 @@
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 
-const Button = () => {
+const Button = ({ href, linkName }) => {
   return (
-    <div className="flex w-full items-center justify-center ls:flex-col ls:pt-10 md:pt-6">
-      <Link href="/portfolio">
-        <a className="projects buttons-color-letter w-48 my-3 text-center mr-2 rounded-3xl py-5 text-indigo-50 font-bold transition-all duration-500 hover:text-blue-50 hover:bg-blue-500 active:transform hover:-translate-y-3 ls:mr-0 ls:mb-2" rel="noreferrer">
-          View Projects
-        </a>
-      </Link>
-      <Link href="/contact">
-        <a className="projects buttons-color-letter w-48 my-3 text-center ml-2 rounded-3xl py-5 text-indigo-50 font-bold transition-all duration-500 hover:text-blue-50 hover:bg-blue-700 active:transform hover:-translate-y-3 ls:ml-0 ls:mt-2" rel="noreferrer">
-          Contact me
-        </a>
-      </Link>
-    </div>
+    <Link href={href}>
+      <a className="projects buttons-color-letter w-48 my-3 text-center ml-2 rounded-3xl py-5 text-indigo-50 font-bold transition-all duration-500 hover:text-blue-50 hover:bg-blue-700 active:transform hover:-translate-y-3 ls:ml-0 ls:mt-2" rel="noreferrer">
+        {linkName}
+      </a>
+    </Link>
   );
 };
 

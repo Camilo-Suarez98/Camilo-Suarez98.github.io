0f967e777ff066ba7e08a20721c4a577f47b2a4c
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-02-22 10:43:39 -0500

    include contact page in project to send messages

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 const contact = () => {
   return (
-    <div>contact</div>
+    <Layout title="Contact">
+      <h1>Contact</h1>
+    </Layout>
   );
 };
 

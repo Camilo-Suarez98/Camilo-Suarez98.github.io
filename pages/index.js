19f000a55565f0755751d791b1e720f402e6d4f1
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-02-23 15:47:41 -0500

    include more text to home page in info

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,9 @@ export default function Home() {
         <div className="flex flex-col justify-around items-center mt-20 h-80 w-full ls:h-5/6 md:mt-16 lg:mt-24 2xl:mt-20">
           <h1 className="font-semibold text-center mb-12 text-4xl text-gray-700 md:mb-4">Hi!👋 I'm Camilo</h1>
           <p className="text-2xl text-gray-500 pb-12 text-center">
-            I'm a Dedicated Frontend Developer located in Bucaramanga, Col 🇨🇴.
+            I'm a dedicated Frontend Developer located in Bucaramanga, Col 🇨🇴.
+            <br />
+            I am always looking for challenges that allow me to keep learning and evolving in this exciting field 💻.
             <br />
             🚀 I specialize in crafting exceptional web experiences, with a focus on user-centric design and robust functionality. My skill set includes: HTML, CSS, JavaScript, React, Next, GIT, TailwindCSS, styled-components and SASS.
           </p>

f4d6dfa504b12b480ca450f3a9de5631a051fc7f
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-02-23 12:04:47 -0500

    change form button color

--- pages/contact.js
+++ pages/contact.js
@@ -33,7 +33,7 @@ const contact = () => {
             />
             <button
               type="submit"
-              className="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150"
+              className="projects buttons-color-letter text-indigo-50 font-bold py-2 px-4 rounded-md mt-4 transition ease-in-out duration-150"
             >
               Send
             </button>

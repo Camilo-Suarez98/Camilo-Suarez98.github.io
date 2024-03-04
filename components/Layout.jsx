fe5a8d5ca2d24e33f874c12e0cc2710ab35a5bc4
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-03-04 09:28:44 -0500

    create new header component to reduce lines in layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 import Image from "next/image";
 import logo from "../public/icono-portf.png";
 import nextIcon from "../public/nextjs-icon.svg";
@@ -8,6 +7,7 @@ import reactQueryIcon from "../public/react-query-icon.svg";
 import tailwindIcon from "../public/tailwind-icon.svg";
 import React, { useContext, useState } from "react";
 import { ThemeContext } from "../context/ThemeContext";
+import Header from "./Header";
 
 const Layout = ({ title, children }) => {
   const { theme, toggleTheme } = useContext(ThemeContext);
@@ -37,61 +37,12 @@ const Layout = ({ title, children }) => {
         <link rel="stylesheet" href="style.css" />
       </Head>
       <div className={theme === 'light' ? 'dark' : 'light'}>
-        <nav className="w-full relative flex flex-wrap items-center justify-between px-2 py-3 mb-10 ls:pb-0 ls:px-0">
-          <div className="container px-12 mx-auto flex items-center justify-between sm:flex-col sm:justify-center ls:px-0">
-            <div className="flex relative px-4 sm:w-auto sm:static sm:justify-center">
-              <a href="/" className="flex items-center text-sm font-bold leading-relaxed cursor-pointer mr-4 py-2 whitespace-nowrap uppercase text-gray-800 sm:mr-0">
-                <Image className="image" src={logo} alt="Page's logo" width={40} height={40} />
-              </a>
-            </div>
-
-            <button onClick={showMenu} className="hidden ls:inline burger" type="button">
-              <span className="block relative w-6 h-px rounded-sm bg-white"></span>
-              <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
-              <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
-            </button>
-
-            <div className="flex flex-grow items-center z-50 transition duration-500 sm:justify-center sm:text-center ls:w-full ls:absolute ls:top-12 ls:-bottom-72 ls:m-auto">
-              <ul className={menu === 'inline' ? 'ls:hidden flex list-none ml-auto transition--all duration-300' : 'menu-burger flex list-none ml-auto transition--all duration-300 ls:flex-col sm:justify-start ls:w-full ls:m-auto ls:items-center'}>
-                <li className="w-full">
-                  <Link href="/" title="Home">
-                    <a className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-gray-800 transition duration-500 hover:text-blue-700 ls:w-full ls:py-5 ls:justify-center">
-                      Home
-                    </a>
-                  </Link>
-                </li>
-                <li className="w-full">
-                  <Link href="/portfolio" title="Portfolio">
-                    <a className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-gray-800 transition duration-500 hover:text-blue-700 ls:w-full ls:py-5 ls:justify-center">
-                      Projects
-                    </a>
-                  </Link>
-                </li>
-                <li className="w-full">
-                  <Link href="/contact" title="Contact">
-                    <a className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-gray-800 transition duration-500 hover:text-blue-700 ls:w-full ls:py-5 ls:justify-center">
-                      Contact
-                    </a>
-                  </Link>
-                </li>
-                <li className="w-full">
-                  <a
-                    href="https://drive.google.com/file/d/1f2toB66E8Neg-LOpEwZ5jpJyhYLsj6eH/view?usp=drive_link"
-                    target="_blank"
-                    className="border-resume px-3 py-2 border-2 sm:border-4 flex items-center text-sm uppercase font-bold leading-snug text-gray-800 transition duration-500 hover:text-blue-700 ls:w-full ls:py-5 ls:justify-center"
-                  >
-                    CV
-                  </a>
-                </li>
-                <li className="w-full px-3 py-2 flex justify-center items-center">
-                  <button onClick={toggleTheme}>
-                    {theme === 'light' ? '🌝' : '🌚'}
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Header
+          showMenu={showMenu}
+          menu={menu}
+          toggleTheme={toggleTheme}
+          theme={theme}
+        />
 
         <div className="w-3/4 m-auto h-full ls:mt-0 flex flex-col justify-evenly flex-wrap">
           {children}

79b80d4b03a0137539b70b6dfec36bc8076e2e03
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-03-07 11:50:44 -0500

    feat: create file with information about projects to use in portfolio and change styles in projects page

---
+++ utils/projects.js
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    id: 1,
+    img: '/project-images/pokedex.png',
+    name: 'PokeDex',
+    description: 'This is a project to show different characters from the famous Pokemon series, you can search by name or by id to see information about any Pokemon.',
+    builtWith: 'React, TailwindCSS and PokeAPI',
+    url: 'https://poke-dex-sable.vercel.app/',
+    repo: 'https://github.com/Camilo-Suarez98/PokeDex'
+  },
+  {
+    id: 2,
+    img: '/project-images/nft.png',
+    name: 'NFT',
+    description: "This is a project to create, sell and bid for nft's by auction, it's possible to create user and login with third party libraries like Google.",
+    builtWith: 'React, Node.JS, Express, SASS, JWT, TypeScript and Prisma as an ORM and PostgreSQL.',
+    url: 'https://nft-marketplace-frontend-one.vercel.app/',
+    repo: 'https://github.com/TatianaIng96/nft-marketplace-frontend'
+  }
+];

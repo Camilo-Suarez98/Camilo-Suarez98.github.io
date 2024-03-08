e1a43769f12c18da01f50b7c3b205b8bdd48bf48
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-03-08 16:20:40 -0500

    feat: change hover style

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import LinkButton from './LinkButton';
 
 const ProjectCard = ({ project }) => {
   return (
-    <div className="projects w-11/12 p-6 self-center justify-self-center transition-all duration-500 rounded-lg h-full hover:border-gray-100 hover:bg-gray-300 hover:-translate-y-3 md:w-72 sm:w-60">
+    <div className="projects w-11/12 p-6 self-center justify-self-center transition-all duration-500 rounded-lg h-full hover:border-gray-100 hover:bg-gray-300 md:w-72 sm:w-60">
       <Image src={project.img} width={450} height={300} priority />
       <h3 className="project-title my-4 text-3xl font-black md:text-xl">{project.name}</h3>
       <p className='language-project flex-grow mb-4 text-lg md:text-sm'>

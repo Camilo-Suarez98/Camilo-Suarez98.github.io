c625fb1ba10fd2a3604699fef329070c9a0eba01
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-03-15 09:50:33 -0500

    feat: change theme by default

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 const ThemeContext = createContext();
 
 function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState('dark');
   const toggleTheme = () => {
     setTheme(theme === 'light' ? 'dark' : 'light')
   };

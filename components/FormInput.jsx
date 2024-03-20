8f4e737924f957a66cfa9247d80a67b5fa43cb7f
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-03-20 10:29:50 -0500

    feat: include useRef to focus on input after first render

--- components/FormInput.jsx
+++ components/FormInput.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { useRef, useEffect } from 'react';
 
 const FormInput = ({ type, name, placeholder }) => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if (name === 'name') {
+      inputRef.current.focus();
+    }
+  }, []);
+
+
   return (
     <>
       <input
         type={type}
         name={name}
+        ref={inputRef}
         placeholder={placeholder}
         className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
         required

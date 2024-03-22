62ae446c9f300c82f9f7b2dcbd4364b9fc288377
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-03-22 10:33:04 -0500

    fix: delete unnecessary text

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="w-3/4 m-auto py-4">
       <p className="text-center">
-        ©2024 Powered by {" "}
+        © Powered by {" "}
         <a
           className="font-black transition duration-500 hover:text-blue-700"
           href="https://github.com/Camilo-Suarez98" target="_blank">

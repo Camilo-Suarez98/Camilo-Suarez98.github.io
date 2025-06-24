68a67ea209e08320d3cbde3f4893581ab486cb52
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2025-06-23 21:36:13 -0500

    change cv link from drive

--- components/Header.jsx
+++ components/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => {
             <NavLink background={pathname === "/portfolio" ? backgroundLink : ""} href="/portfolio">Projects</NavLink>
             <li className="w-full">
               <a
-                href="https://drive.google.com/file/d/1_Y1KwhGc3zwi8ElA7wabM1GcRuAQia0g/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1HaaEWzGIoIe0r7qi42Da-22_oMI7yNvP/view?usp=drive_link"
                 target="_blank"
                 className="border-resume px-3 py-2 border-2 sm:border-4 flex items-center text-sm uppercase font-bold leading-snug text-gray-800 transition duration-500 hover:text-blue-700 ls:w-full ls:py-5 ls:justify-center"
               >

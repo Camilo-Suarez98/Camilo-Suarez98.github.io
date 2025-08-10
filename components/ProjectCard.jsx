753d2e2c3e93461a1618a200af38ae3314dee673
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2025-08-10 15:29:48 -0500

    change styles in project card and buttons

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,18 +1,21 @@
 import Image from 'next/image';
 
 import LinkButton from './LinkButton';
+import Link from 'next/link';
 
 const ProjectCard = ({ project }) => {
   return (
-    <div className="projects w-11/12 p-6 self-center justify-self-center transition-all duration-500 rounded-3xl h-full hover:border-gray-100 sm:w-80 sm:p-3 md:w-72">
-      <Image
-        src={project.img}
-        width={450}
-        height={300}
-        alt={`Image from project called: ${project.name}`}
-        className="h-80 w-full object-fill rounded-3xl sm:h-52"
-        priority
-      />
+    <div className="projects w-11/12 shadow-xl p-6 self-center justify-self-center transition-all duration-500 rounded-xl h-full hover:border-gray-100 sm:w-80 sm:p-3 md:w-72">
+      <Link href={project.url} target='_blank'>
+        <Image
+          src={project.img}
+          width={450}
+          height={300}
+          alt={`Image from project called: ${project.name}`}
+          className="h-80 w-full object-fill rounded-xl sm:h-52"
+          priority
+        />
+      </Link>
       <h3 className="project-title my-4 text-3xl font-black md:text-xl">{project.name}</h3>
       <p className='language-project flex-grow mb-4 text-lg md:text-sm'>
         {project.description}
